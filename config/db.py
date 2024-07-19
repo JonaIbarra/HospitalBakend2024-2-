6fc80532b9477508305f63a3969de3ba59bcba6f
JonaIbarra <ei9063584@gmail.com>
2024-07-19 17:04:28 -0600

    update

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_TBYQRw8Y7ip1pTE1On0@mysql-3ced59d1-ei9063584-e1b0.h.aivencloud.com:22520/defaultdb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
